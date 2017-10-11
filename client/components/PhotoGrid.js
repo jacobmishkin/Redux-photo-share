377b428f9807cc4e3d03c43efc3e2df97e73d6ca
Jacob Mishkin <user@MacBook-Pro.local>
2017-10-11 12:09:02 -0500

    inital components and router created

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+class PhotoGrid extends React.Component {
+
+		render() {
+		return (
+			<div className="photo-grid">
+					this is the Grid
+			</div>
+		)
+	}
+}
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+class Single extends React.Component {
+
+		render() {
+		return (
+			<div className="single-photo">
+				this is the single
+			</div>
+		)
+	}
+}
+
+export default Single;
